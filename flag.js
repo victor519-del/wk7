9bb1a06c4c6a6a0fb369d22e72ed171491f1d4c7
victor519-del! <victoroladimeji519@gmail.com>
2023-06-13 20:53:29 +0100

    feat: add a file

---
+++ flag.js
@@ -0,0 +1,34 @@
+// const { name } = require("commander")
+
+// let myName = 'victor'
+
+// myName = 'VICTOR'
+
+// const school ='Univelcity'
+
+// const state ='Kwara state'
+
+// const statement = `MY NAME IS ${myName} and i work at ${school} and i am from ${state}`
+
+// console.log(statement)
+
+// const statement2 = 'A wise man once said "Suffer now,and lives the rest of your life a champion"'
+
+// console.log(statement2)
+
+// let age = prompt(`how old are you`)
+
+// console.log(`You are born in ${new Date().getFullYear() - age}`)
+
+// confirm(statement2)
+// alert(statement)
+
+const statement3
+
+confirm(statement3)
+
+console.log (statement3)
+
+let victor = prompt(`What is ur name`)
+
+// console.log(`YOU ARE BORN IN ${new Date().getDay() - age}`)
---
+++ lag.js
@@ -0,0 +1,5 @@
+let yourName = prompt('what is ur name')
+
+let age = prompt('What year were you Born')
+
+const statement = ('your name is ${yourName}, and you ${new date().getFullYear() - age} year old')
